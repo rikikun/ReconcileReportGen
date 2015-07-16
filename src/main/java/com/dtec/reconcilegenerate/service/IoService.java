0268d2208eb1d431d27f00c3e8c2da2546508293
rikikun <pondpond_arsenal@hotmail.com>
2015-07-16 15:56:37 +0700

    create function ioService

--- src/main/java/com/dtec/reconcilegenerate/service/IoService.java
+++ src/main/java/com/dtec/reconcilegenerate/service/IoService.java
@@ -5,9 +5,11 @@
  */
 package com.dtec.reconcilegenerate.service;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -16,14 +18,27 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  * @author rikikun
  */
 public class IoService {
-    	public void writeExcel(XSSFWorkbook workBook,String fileName) throws IOException{
-		FileOutputStream outPut = new FileOutputStream(new File(fileName));
-		workBook.write(outPut);
-		outPut.close();
-	}
-	
-	public XSSFWorkbook readExcel(String fileName) throws IOException{
-		FileInputStream file = new FileInputStream(new File(fileName));
-		return new XSSFWorkbook(file);
-	}
+
+    public void writeExcel(XSSFWorkbook workBook, String fileName) throws IOException {
+        FileOutputStream outPut = new FileOutputStream(new File(fileName));
+        workBook.write(outPut);
+        outPut.close();
+    }
+
+    public XSSFWorkbook readExcel(String fileName) throws IOException {
+        FileInputStream file = new FileInputStream(new File(fileName));
+        return new XSSFWorkbook(file);
+    }
+
+    public String readSql(String sqlName) throws IOException {
+        BufferedReader input =new BufferedReader(new FileReader(sqlName));
+        String sql="";
+        sql+=input.readLine();
+        return sql;
+    }
+    
+    public Boolean checkFileExist(String fileName){
+        File file=new File(fileName);
+        return file.exists();
+    }
 }
